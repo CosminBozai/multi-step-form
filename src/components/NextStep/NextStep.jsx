c34274f2d2521b7fa5ad24aaf57e3b476a9be4a2
Bozai Cosmin <cosminbozai@outlook.com>
2023-02-22 14:12:11 +0000

    Styled the next step component

---
+++ src/components/NextStep/NextStep.jsx
@@ -0,0 +1,12 @@
+import "./NextStep.scss";
+
+function NextStep() {
+  return (
+    <div className="next-step-container">
+      <button className="go-back">Go back</button>
+      <button>Next Step</button>
+    </div>
+  );
+}
+
+export default NextStep;
